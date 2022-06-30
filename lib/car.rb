bfc142a636609ebfdc01e6f7a3a17b1b49a9d9e5
Connie Park <conniedc1206@gmail.com>
2022-06-30 14:24:30 -0700

    commit tree 7527e82668f285787654ad662f5223b8f6c7c4be

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,10 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+# Step 2: Defining the Subclass
+class Car < Vehicle
+    # Step 3: Overwriting Inherited Methods
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,19 @@
+# Step 1: Defining the Super Class
 class Vehicle
+    # Instances of Vehicle initialize with a wheel size and number. 
+    attr_accessor :wheel_size, :wheel_number
 
+    def initialize(wheel_size, wheel_number)
+      @wheel_size = wheel_size
+      @wheel_number = wheel_number
+    end
+    
+    # We also have #go and #fill_up_tank instance methods that describe some common vehicle behavior.
+    def go
+      "vrrrrrrrooom!"
+    end
+  
+    def fill_up_tank
+      "filling up!"
+    end
 end
